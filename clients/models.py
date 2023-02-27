7ea391b1eecab5f4c426ccfe5964a97278dce10a
CaDiBob <nikolay.v.byankin@gmail.com>
2023-02-27 20:16:42 +0900

    added field first_name to Client model

--- clients/models.py
+++ clients/models.py
@@ -7,10 +7,11 @@ class Client(models.Model):
         unique=True,
         db_index=True
     )
+    first_name = models.CharField('Имя', max_length=50)
 
     class Meta:
         verbose_name = 'Клиент'
         verbose_name_plural = 'Клиенты'
 
     def __str__(self):
-        return f'{self.tg_user_id}'
+        return f'{self.tg_user_id} {self.first_name}'
